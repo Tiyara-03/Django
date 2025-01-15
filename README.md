6d8b66a628eef6b9c08762febd15b2537fe5bd73
Tiyara <tiara26dia37@gmail.com>
2025-01-15 23:28:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django
+ This repository is documented learning process of Django is my Web Developemnt course.
---
+++ gswDjango/gswDjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+
+def home(request):
+    # return HttpResponse("Hello, World, This is Home Page")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("Hello, World. You are at tiya's About Page")
+
+def contact(request):
+    return HttpResponse("Hello, This is contact Page")

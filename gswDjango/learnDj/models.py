60b628e79ebe923058489c7f1f41871fb6704317
Tiyara <tiara26dia37@gmail.com>
2025-02-12 21:52:46 +0530

    ..

--- gswDjango/learnDj/models.py
+++ gswDjango/learnDj/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class LearnDjnago (models.Model):
+    LEARN_DJANGO = [
+        ('ML', 'MASALA'),
+        ('GR', 'GINGER'),
+        ('KL', 'KIWI'),
+        ('PL', 'PLAIN'),
+        ('EL', 'ELACHI'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default= timezone.now)
+    type = models.CharField(max_length=2, choices = LEARN_DJANGO)
+
